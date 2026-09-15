133467d3bcb8291b40ede11764dcd760ffa0f1a5
boddycarey-design <boddycarey@gmail.com>
2026-09-15 16:37:34 -0500

    Defined all subfunctions
    preliminary step to define all subfunctions to just get something on the board

--- subfunctions.py
+++ subfunctions.py
@@ -1,5 +1,35 @@
+#import libraries#
+import numpy as np 
+import matplotlib.pyplot as plt
+import math as mp
+
+
+def tau_dcmotor():
+    """Computes the torque of the DC motor"""
+    print("Torque of DC motor")
+
+def get_gear_ratio():
+    """Computes the gear ratio of the rover"""
+    print("Gear ratio")
+
 def get_mass():
     """Computes the mass of the rover"""
     print("Mass")
+
+def F_drive():
+    """Computes the driving force of the rover"""
+    print("Driving force")
+
+def F_gravity():
+    """Computes the gravitational force acting on the rover"""
+    print("Gravitational force")
+
+def F_rolling():
+    """Computes the rolling resistance force acting on the rover"""
+    print("Rolling resistance force")
+
+def F_net():
+    """Computes the net force acting on the rover"""
+    print("Net force")
 
 
