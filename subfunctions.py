57f8cf2d2e7e632e50395c3e7ee9b3496b77d90d
boddycarey <boddycarey@gmail.com>
2026-09-15 17:33:33 -0500

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -5,31 +5,58 @@ import math as mp
 
 
 def tau_dcmotor():
-    """Computes the torque of the DC motor"""
-    print("Torque of DC motor")
+    """Returns  the  motor  shaft  torque  when  given  motor  shaft  speed  and  a  dictionary  containing 
+    important specifications for the motor"""
+
+    """returns 
+        - the motor shaft torque in Nm given the shaft speed in rad/s 
+        - the motor specifications structures"""
+    
 
 def get_gear_ratio():
-    """Computes the gear ratio of the rover"""
-    print("Gear ratio")
+    """Returns the speed reduction ratio for the speed reducer based on speed_reducer dict."""
+    
 
-def get_mass():
-    """Computes the mass of the rover"""
-    print("Mass")
+def get_mass(rover):
+    """Computes the total mass of the rover. Uses information in the rover dict."""
+
+    #rover dictionary for all masses of the rover components
+    #this would find "chassis" in the rover dictionary and then look at its "mass"
+    chassis_mass = rover['chassis']['mass'] 
+    power_subsystem_mass = rover['power_subsystem']['mass']
+    payload_mass = rover['payload']['mass']
+
+    #subdictionary for the wheel assembly that contians the motor, speed reducer, and the wheels.
+    motor_mass = rover['wheel_assembly']['motor']['mass']
+    speed_reducer = rover['wheel_assembly']['speed_reducer']['mass']
+    wheel_mass = rover['wheel_assembly']['wheel']['mass']
+
+    #multiple the wheel assembly mass by 6 because there are 6 wheels on the rover
+    wheel_assembly_mass = 6*(motor_mass + speed_reducer + wheel_mass)
+
+    mass_total = chassis_mass + power_subsystem_mass + payload_mass + wheel_assembly_mass
+    return mass_total
+
 
 def F_drive():
-    """Computes the driving force of the rover"""
-    print("Driving force")
+    """Returns the force applied to the rover by the drive system given information about the drive 
+    system (wheel_assembly) and the motor shaft speed. """
+    
 
 def F_gravity():
-    """Computes the gravitational force acting on the rover"""
-    print("Gravitational force")
+    """Returns  the  magnitude  of  the  force  component  acting  on  the  rover  in  the  direction  of  its 
+    translational  motion  due  to  gravity  as  a  function  of  terrain  inclination  angle  and  rover 
+    properties. """
+    
 
 def F_rolling():
-    """Computes the rolling resistance force acting on the rover"""
-    print("Rolling resistance force")
+    """Returns  the  magnitude  of  the  force  acting  on  the  rover  in  the  direction  of  its  translational 
+    motion  due  to  rolling  resistances  given  the  terrain  inclination  angle,  rover  properties,  and  a 
+    rolling resistance coefficient. """
+    
 
 def F_net():
-    """Computes the net force acting on the rover"""
-    print("Net force")
-
+    """Returns  the  magnitude  of  net  force  acting  on  the  rover  in  the  direction  of  its  translational 
+    motion."""
+    
 
