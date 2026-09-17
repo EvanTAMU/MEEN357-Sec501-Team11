91da5adce56ab2cc844eb6333df01e37c8a8f64a
Collin Sun <collins3412@tamu.edu>
2026-09-17 12:09:28 -0500

    commit

--- analysis_terrain_slope.py
+++ analysis_terrain_slope.py
@@ -1,1 +1,38 @@
+#call force drive, force gravity, force rolling
+#input inclination angle, rover properties, rolling coefficient(.15)
+#call defined function net force
+
+import math
+import numpy as np
+#initialize bounds
+def bisec(F_net, xl = 0, xu = 2*math.pi, errTol = 1e-6, iterMax=100, funTol=1e-6)
+    # xl = 0
+    # xu = 2*math.pi
+
+    iter = 0
+    done = False
+
+    # err_Tol = 1e-6
+    # iterMax = 100
+    # fun_Tol = 1e-6
+
+    while not done:
+        iter += 1
+
+        x_bisect = (xu-xl)/2
+
+        fu = F_net(xu)
+        fl = F_net(xl)
+        f_bisect = F_net(x_bisect)
+
+        if fl * f_bisect < 0:
+            fu = f_bisect
+        else:
+        fl = f_bisect
+
+        error_est = abs((xu-xl)/xr)*100
+        if (error_est < errTol) or iterNum >= iterMax or (abs(f_bisect) < funTol):
+            done = True
+        
+
 
