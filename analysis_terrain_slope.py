8feae3cb150c992f336bf73ca7b732f86f1c6dd1
boddycarey <boddycarey@gmail.com>
2026-09-19 16:20:45 -0500

    terrain slope analysis finished

--- analysis_terrain_slope.py
+++ analysis_terrain_slope.py
@@ -1,73 +1,93 @@
-import math
-import numpy as np
+#import library functions
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import bisect
 
-#import necessary subfunctions 
-from subfunctions import get_gear_ratio 
+#import necessary subfunctions
+from subfunctions import get_gear_ratio, F_net
+
+# define rover and planet dictionaries # used AI to create rover dictionary
+rover = {
+    'wheel_assembly': {
+        'wheel': {
+            'radius': 0.3,    # Radius of drive wheel [m]
+            'mass': 1.0       # Mass of one drive wheel [kg]
+        },
+        'speed_reducer': {
+            'type': 'reverted',  # Reducer type (reverted gear train)
+            'diam_pinion': 0.04, # Pinion diameter d1 [m]
+            'diam_gear': 0.07,   # Gear diameter d2 [m]
+            'mass': 1.5          # Mass of speed reducer assembly [kg]
+        },
+        'motor': {
+            'torque_stall': 170.0,  # Motor stall torque [N-m]
+            'torque_noload': 0.0,   # Motor no-load torque [N-m]
+            'speed_noload': 3.80,   # Motor no-load speed [rad/s]
+            'mass': 5.0             # Mass of motor [kg]
+        }
+    },
+    'chassis': {
+        'mass': 659.0  # Mass of rover chassis [kg]
+    },
+    'science_payload': {
+        'mass': 75.0   # Combined mass of all scientific instruments [kg]
+    },
+    'power_subsys': {
+        'mass': 90.0   # Mass of RTG power subsystem [kg]
+    }
+}
+
+
+planet = {
+    'g':3.72 # acceleration due to gravity on Mars
+    }
+
+speed_reducer = {
+    'reverted'
+}
 
 
 #set parameters
-slope_array_degs = np.linspace(-15,35,25)
+slope_array_deg = np.linspace(-15,35,25)
 Crr = .015
 
+#define more parameters for bisect 
+left = 0  
+right = rover['wheel_assembly']['motor']['speed_noload']  
+radius = rover['wheel_assembly']['wheel']['radius']
+
+# call function to get the gear ratio Ng for converting omega to velocity
+Ng = get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
+
 #store vmax values
-vmax_values = []
+v_max = []
 
-for slope_array_deg in slope_array_degs:
+for slope_deg in slope_array_deg : 
 
-    #find omega when net force equals 0
-    f = lambda omega: F_net(omega, terrain_angle, rover, planet, Crr)
+    force = lambda omega: F_net(omega, slope_deg, rover, planet, Crr)
 
-    #bisect for omega root
-    omega_root = bisect(omega, 0, 3.8)
-    #convert omega when net force is 0 to translational velocity
-    #w_wheel = w_shaft/gear_ratio
-    #r = .3m
-    #v = w_wheel * r
-    v_max = (omega_root/get_gear_ratio(speed_reducer))*(.3)
+    try: 
+        #find the root using bisect 
+        omega_root = bisect(force, left, right)
 
-    #append each vmax throughout the for loop
-    vmax_values.append(v_max)
+        #convert the root from angular to translational
+        v_max_value = radius*(omega_root/Ng)
+
+        v_max.append(v_max_value)
+   
+    except : 
+        # append a null value if no root present
+        v_max.append(np.nan) 
 
 
 #plot vmax vs slope
-plt.plot(vmax_values,slope_array_degs)
+plt.plot(v_max,slope_array_deg)
 plt.xlabel('Incline Angle: degrees')
 plt.ylabel('Maximum Velocity: m/s')
+plt.grid(True)
+plt.show()
 
 
-
-
-#SCRATCH
-# #initialize bounds
-# def bisec(F_net, xl = 0, xu = 2*math.pi, errTol = 1e-6, iterMax=100, funTol=1e-6)
-#     # xl = 0
-#     # xu = 2*math.pi
-
-#     iter = 0
-#     done = False
-
-#     # err_Tol = 1e-6
-#     # iterMax = 100
-#     # fun_Tol = 1e-6
-
-#     while not done:
-#         iter += 1
-
-#         x_bisect = (xu-xl)/2
-
-#         fu = F_net(xu)
-#         fl = F_net(xl)
-#         f_bisect = F_net(x_bisect)
-
-#         if fl * f_bisect < 0:
-#             fu = f_bisect
-#         else:
-#         fl = f_bisect
-
-#         error_est = abs((xu-xl)/xr)*100
-#         if (error_est < errTol) or iterNum >= iterMax or (abs(f_bisect) < funTol):
-#             done = True
         
 
 
